ce71eaa94d80fb735e06a7e3775ca2a559e1ca1b
gary <gary.white@lodgit.com>
2022-07-05 13:25:52 +0200

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function alternate(n, firstValue, secondValue) {
+    // your code
+}
+
+console.log(alternate(5, true, false)); // [true, false, true, false, true]
+console.log(alternate(20, 'blue', 'red')); // ['blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red', 'blue', 'red']
+console.log(alternate(0, "lemons", "apples")); // []
