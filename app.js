8a3c9e6702ec70373e9d15f7e28575249c6c759f
gary <gary.white@lodgit.com>
2022-07-05 13:50:51 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 function alternate(n, firstValue, secondValue) {
-    // your code
+    var myArr = [];
+    for (let i = 0; i < n; i++) {
+        myArr.push(firstValue, secondValue);
+    }
+    var half = Math.ceil(myArr.length / 2)
+    return myArr.splice(0, half);
 }
 
 console.log(alternate(5, true, false)); // [true, false, true, false, true]
